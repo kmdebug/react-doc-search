39f8bc4d1167aeffe64d9a834edc8e80f7e79e17
kmdebug <kaissar.mouelhi@outlook.com>
2025-01-27 17:25:28 +0000

    chore(app): enhance layout and update scrollbar styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 const App: React.FC = () => {
   return (
-    <div>
-      <h1>React App</h1>
+    <div
+      className="bg-[#f9fbfd]"
+      style={{
+        padding: '11px 0px 0px',
+      }}
+    >
+      <div
+        className="bg-white w-[804px] h-[1201px] border mx-auto p-[68px] text-[15px] font-sans leading-normal font-normal"
+        style={{
+          boxShadow:
+            'rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.20) 0px 0px 0px 0.7px',
+        }}
+      >
+        <h1 className="text-black">React App</h1>
+      </div>
     </div>
   );
 };
