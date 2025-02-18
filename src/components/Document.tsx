d36ed235b519663bdb2c90e3f2e879ae87418058
kmdebug <kaissar.mouelhi@outlook.com>
2025-02-18 11:29:45 +0000

    fix(document): fix font to arial

--- src/components/Document.tsx
+++ src/components/Document.tsx
@@ -7,7 +7,7 @@ type Props = {
 const Document: React.FC<Props> = ({ children }): JSX.Element => {
   return (
     <div className="bg-[#f9fbfd] m-[11px]">
-      <div className="bg-white max-w-[804px] h-[1201px] border mx-auto p-[68px] text-[15px] font-sans leading-normal font-normal shadow-[0px_1px_3px_0px_rgba(0,_0,_0,_0.02),_0px_0px_0px_0.7px_rgba(27,_31,_35,_0.20)]">
+      <div className="bg-white max-w-[804px] h-[1201px] border mx-auto p-[68px] text-[15px] font-[Arial] leading-normal font-nsormal shadow-[0px_1px_3px_0px_rgba(0,_0,_0,_0.02),_0px_0px_0px_0.7px_rgba(27,_31,_35,_0.20)]">
         <p className="whitespace-pre-wrap text-black">{children}</p>
       </div>
     </div>
